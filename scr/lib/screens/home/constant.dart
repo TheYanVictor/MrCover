095971284412f6a9d04cfda6af958b1bdf984090
Yan Victor <106403574+TheYanVictor@users.noreply.github.com>
2023-12-03 17:18:03 -0300

    Prototype done

--- scr/lib/screens/home/constant.dart
+++ scr/lib/screens/home/constant.dart
@@ -3,3 +3,7 @@ import 'package:flutter/material.dart';
 const kPrimaryColor = Color.fromARGB(255, 250, 174, 34);
 const kTextColor = Color(0xFF3C4046);
 const kDarkButtonColor = Color(0xFF0D0D0D);
+
+// Text values
+var encryptText = TextEditingController();
+var decryptText = "Decrypt text will appear here:";
--- scr/lib/screens/home/home_screen.dart
+++ scr/lib/screens/home/home_screen.dart
@@ -1,4 +1,7 @@
+// ignore_for_file: prefer_const_constructors
+
 import 'package:flutter/material.dart';
+import 'package:scr/screens/home/constant.dart';
 
 class HomeScreen extends StatefulWidget {
   const HomeScreen({super.key});
@@ -13,17 +16,172 @@ class _HomeScreenState extends State<HomeScreen> {
     Size size = MediaQuery.of(context)
         .size; //This provides us total height and width of our screen
     return Scaffold(
-      body: Container(
-        color: Colors.red,
-        height: size.height,
-        width: size.width,
-        child: Stack(
+      backgroundColor: Colors.indigo[900],
+      body: Padding(
+        padding: EdgeInsets.all(8.0),
+        child: Column(
           children: [
-            Center(
-              child: Text(
-                "Mr Cover",
-                style: TextStyle(color: Colors.white),
-              ),
+            //Welcome Text
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                //Title
+                Column(
+                  children: [
+                    //Space
+                    Container(
+                      height: size.height * 0.03,
+                    ),
+                    //Title
+                    Text(
+                      "Mr. Cover",
+                      style: TextStyle(
+                        fontSize: size.width * 0.05,
+                        fontWeight: FontWeight.bold,
+                        color: Colors.white,
+                      ),
+                    ),
+                    //Subtitle
+                    Text(
+                      "Keep your secrets safe",
+                      style: TextStyle(
+                        fontSize: size.width * 0.02,
+                        fontWeight: FontWeight.bold,
+                        color: Colors.white,
+                      ),
+                    ),
+                  ],
+                ),
+              ],
+            ),
+            //Space
+            Container(
+              height: size.height * 0.18,
+            ),
+            //Message to encrypt
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: [
+                //Message to encrypt
+                Container(
+                  height: size.height * 0.3,
+                  width: size.width * 0.3,
+                  decoration: BoxDecoration(
+                    color: Colors.white,
+                    borderRadius: BorderRadius.circular(10),
+                  ),
+                  child: TextField(
+                    textAlign: TextAlign.justify,
+                    controller: encryptText,
+                    decoration: InputDecoration(
+                      labelStyle: TextStyle(
+                        fontSize: size.width * 0.02,
+                        fontFamily: "Arial",
+                        color: Colors.black26,
+                      ),
+                      border: InputBorder.none,
+                      labelText: "Message to encrypt:",
+                      contentPadding: EdgeInsets.all(20),
+                    ),
+                  ),
+                ),
+                //Encrypt button
+                Container(
+                  height: size.height * 0.1,
+                  width: size.width * 0.1,
+                  decoration: BoxDecoration(
+                    color: Colors.white,
+                    borderRadius: BorderRadius.circular(10),
+                  ),
+                  child: IconButton(
+                    icon: Icon(Icons.arrow_forward),
+                    onPressed: () {
+                      setState(
+                        () {
+                          decryptText = encryptText.text;
+                        },
+                      );
+                    },
+                  ),
+                ),
+                //Decrypt message
+                Container(
+                  height: size.height * 0.3,
+                  width: size.width * 0.3,
+                  decoration: BoxDecoration(
+                    color: Colors.white,
+                    borderRadius: BorderRadius.circular(10),
+                  ),
+                  child: TextField(
+                    readOnly: true,
+                    textAlign: TextAlign.justify,
+                    decoration: InputDecoration(
+                      labelStyle: TextStyle(
+                        fontSize: size.width * 0.02,
+                        fontFamily: "Arial",
+                        color: Colors.black26,
+                      ),
+                      border: InputBorder.none,
+                      labelText: decryptText,
+                      contentPadding: EdgeInsets.all(20),
+                    ),
+                  ),
+                ),
+              ],
+            ),
+            //Space
+            Container(
+              height: size.height * 0.05,
+            ),
+            // Copy public and private keys to clipboard
+            Row(
+              mainAxisAlignment: MainAxisAlignment.end,
+              children: [
+                //Copy public key
+                Container(
+                  height: size.height * 0.05,
+                  width: size.width * 0.05,
+                  decoration: BoxDecoration(
+                    color: Colors.white,
+                    borderRadius: BorderRadius.circular(10),
+                  ),
+                  child: IconButton(
+                    tooltip: "Public key",
+                    icon: Icon(Icons.copy),
+                    onPressed: () {
+                      setState(
+                        () {
+                          decryptText = encryptText.text;
+                        },
+                      );
+                    },
+                  ),
+                ),
+                //Copy private key
+                Container(
+                  height: size.height * 0.05,
+                  width: size.width * 0.05,
+                  decoration: BoxDecoration(
+                    color: Colors.white,
+                    borderRadius: BorderRadius.circular(10),
+                  ),
+                  child: IconButton(
+                    tooltip: "Private key",
+                    icon: Icon(Icons.copy),
+                    onPressed: () {
+                      setState(
+                        () {
+                          decryptText = encryptText.text;
+                        },
+                      );
+                    },
+                  ),
+                ),
+                //Space
+                SizedBox(
+                  width: size.width * 0.07,
+                ),
+              ],
             ),
           ],
         ),
